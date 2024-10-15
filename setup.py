822805d3e4880fcc9df2091ae67f8339d0005c49
Brandon Elias <brandonelias09@gmail.com>
2024-10-15 18:19:50 -0400

    Update package structure and restore necessary files

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='HistoricalEarningsData',  # New name with proper capitalization
-    version='0.4',  # Increment the version number
+    name='HistoricalEarningsData',  # Updated name
+    version='0.5',                   # Incremented version
     description='A package to query historical earnings report dates for stock tickers',
     author='Your Name',
     author_email='your.email@example.com',
-    url='https://github.com/yourusername/historical_earnings_package',  # Link to your GitHub repo
+    url='https://github.com/yourusername/historical_earnings_package',
     packages=find_packages(),
     include_package_data=True,
     package_data={
-        'HistoricalEarnings': ['data/aggregated_earnings_data_webscraped.csv'],  # Use new name here
+        'HistoricalEarningsData': ['data/aggregated_earnings_data_webscraped.csv'],
     },
     install_requires=[
         'pandas',
